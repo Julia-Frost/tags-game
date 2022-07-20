a7a7bc584e874171fdb7dbd72e2d152756e6bcd4
Julia-Frost <juliafrostwork@gmail.com>
2022-07-20 21:09:47 +0300

    add game core

---
+++ js/script.js
@@ -0,0 +1,53 @@
+let emptyX = 3;
+let emptyY = 3;
+
+// Цвет ячейки
+
+function moveToEmptyPlace(cell) {
+  const newX = emptyX;
+  const newY = emptyY;
+
+  emptyX = cell.x;
+  emptyY = cell.y;
+
+  cell.x = newX;
+  cell.y = newY;
+
+  cell.style.left = `${newX * 25}%`;
+  cell.style.top = `${newY * 25}%`;
+}
+
+let cells = [...document.querySelectorAll(".cell")];
+cells.sort(() => Math.random() - 0.5);
+
+let x = 0;
+let y = 0;
+
+for (let cell of cells) {
+  cell.x = x;
+  cell.y = y;
+  moveToEmptyPlace(cell);
+
+  x += 1;
+
+  if (x > 3) {
+    y += 1;
+    x = 0;
+  }
+
+  cell.onclick = () => {
+    let dX = Math.abs(cell.x - emptyX);
+    let dY = Math.abs(cell.y - emptyY);
+    let distance = dX + dY;
+
+    if (distance < 2) {
+      moveToEmptyPlace(cell);
+    }
+  };
+}
+
+let cellsColor = [...document.querySelectorAll(".cell")];
+// const colors = ["red", "yellow", "green", "blue"];
+// const randomColor = getRandomNumber(0, colors.length - 1);
+// circle.style.background = `${colors[randomColor]}`;
+// return Math.round(Math.random() * (max - min) + min);
